3d6eee9afa8db2859b87e582c03ab0a991fb0d38
csielee <lee097019@gmail.com>
2017-12-11 00:05:10 +0800

    Add setting ae at editor and ae record last config

--- ae.js
+++ ae.js
@@ -1,14 +1,40 @@
 module.exports = function(RED) {
+    var last_config = {};
+    var count = 0;
     RED.nodes.registerType("AE",function (config) {
         RED.nodes.createNode(this,config);
 
-        this.log("create ae");
+        count++;
+        if (!last_config.hasOwnProperty(config.id)) {
+            //first create
+        }
+        else {
+            //do something
+            this.log('last')
+            this.log(JSON.stringify(last_config[config.id]));
+            this.log('now')
+            this.log(JSON.stringify(config));
+        }
+        last_config[config.id] = Object.assign({},config);
+
+        this.log("create ae by "+ count);
         this.cse = RED.nodes.getNode(config.cse);
         //this.cse.retrieveResource('').then(data=>this.log(JSON.stringify(data)));
         this.cse.createResource('', 'AE', {"m2m:ae":{
-            'api' : config.name,
+            'api' : config.appID,
+            'lbl' : config.labels,
+            'rn' : config.name,
+            'apn' : config.appName,
             'rr' : false,
         }}).then(data=>{this.log(JSON.stringify(data)); this.info = data});
 
+        this.on('close', function(removed, done) {
+            if (removed) {
+                // node be deleted
+            } else {
+                // just restart
+            }
+            done();
+        })
     });
 }
