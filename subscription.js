1fffe4c4dfe0dfe8ec3645ee0f55224cf4bd06d9
csielee <lee097019@gmail.com>
2017-12-22 16:52:19 +0800

    Add promise to get ngrok url and delete when node close

--- subscription.js
+++ subscription.js
@@ -6,6 +6,24 @@ module.exports = function(RED) {
     subscriptionApp.use(bodyParser.json());
     subscriptionApp.use(bodyParser.urlencoded({ extended: false }))
     var ngrokUrl = undefined;
+    RED.settings.set('ngrokUrl', ngrokUrl);
+    function getNgrokURL() {
+        return new Promise((resolve, reject) => {
+            var timeOutCount = 0;
+            var loop = () => setTimeout(()=>{
+                timeOutCount++;
+                if (RED.settings.get('ngrokUrl') !== undefined) {
+                    resolve(RED.settings.get('ngrokUrl'))
+                } else {
+                    if (timeOutCount > 20)
+                        reject("ngrok url timeout");
+                    else
+                        loop();
+                }
+            }, 100);
+            loop();
+        })
+    }
 
     RED.nodes.registerType("subscription",function (config) {
         RED.nodes.createNode(this,config);
@@ -13,30 +31,68 @@ module.exports = function(RED) {
         if (!config.url)
             return;
 
-        request({
-            url : config.url,
-            method : 'POST',
-            headers : {
-                'X-M2M-Origin' : 'admin:admin',
-                'Content-Type' : 'application/json;ty=23',    
-            },
-            body : {
-                "m2m:sub": {
-                    "rn" : this.id,
-                    "nu" : ngrokUrl + '/sub/' + this.id,
-                    "nct" : 2,
+        this.hasCreate = false;
+
+        getNgrokURL().then(url=>{
+            this.log('get ngrok url : '+url)
+            request({
+                url : config.url,
+                method : 'POST',
+                headers : {
+                    'X-M2M-Origin' : 'admin:admin',
+                    'Accept' : 'application/json',
+                    'Content-Type' : 'application/json;ty=23',    
+                },
+                body : {
+                    "m2m:sub": {
+                        "rn" : this.id,
+                        "nu" : url + '/sub/' + this.id,
+                        "nct" : 2,
+                    }
+                },
+                rejectUnauthorized: false,
+                json : true,
+            },(error, response, body)=>{
+                if (error)
+                    this.error(error)
+                else if (typeof body == "string") {
+                    this.error(body)
+                } else {
+                    this.log('create sub : ' + JSON.stringify(body))
+                    this.hasCreate = true;
                 }
-            },
-            rejectUnauthorized: false,
-            json : true,
-        },(error, response, body)=>{
-            if (error)
-                this.error(error)
-            else if (typeof body == "string") {
-                this.error(body)
+            })
+        }).catch(error => this.error(error))
+
+        this.on('close', function(done) {
+            this.log('close sub')
+            if (this.hasCreate) {
+                // need to delete sub
+                request({
+                    url : config.url + '/' + this.id,
+                    method : 'DELETE',
+                    headers : {
+                        'X-M2M-Origin' : 'admin:admin',
+                        'Accept' : 'application/json',
+                        'Content-Type' : 'application/json;ty=23',    
+                    },
+                    body : {},
+                    rejectUnauthorized: false,
+                    json : true,
+                },(error, response, body)=>{
+                    if (error)
+                        this.error(error)
+                    else if (typeof body == "string") {
+                        this.error(body)
+                    } else {
+                        this.log('delete sub : ' + JSON.stringify(body))
+                        //this.hasCreate = true;
+                    }
+                    done();
+                })
             } else 
-                this.log(JSON.stringify(body))
-        })
+                done();
+        });
     });
     subscriptionApp.use('/:id',(req, res)=>{
         var subNode = RED.nodes.getNode(req.params.id);
@@ -61,6 +117,7 @@ module.exports = function(RED) {
         else {
             RED.log.info(`[ngrok] http:127.0.0.1:${RED.settings.get('uiPort')} -> ${url}`)
             ngrokUrl = url.replace("https","http");
+            RED.settings.set('ngrokUrl', ngrokUrl);
         }
         request.debug = true;
     })
